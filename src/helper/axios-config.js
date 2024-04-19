3ef9205a398773dcddbaa1f86687b8495cf888b0
javmar04@yahoo.es <javmar04@yahoo.es>
2024-04-19 17:28:16 -0500

    cambio baseURL de backend

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
     //baseURL: 'http://localhost:4000'
 // baseURL: 'https://videos-backend5.onrender.com'
-baseURL: 'https://video-bk.onrender.com'
+baseURL: 'https://video-backend-pbvy.onrender.com'
     
 });
 
